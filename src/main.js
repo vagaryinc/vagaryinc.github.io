0664ccffd291a43ad72d61bd66b253f0c649605f
justinmandac <justinmandac@gmail.com>
2017-08-23 19:09:30 +0800

    Update

---
+++ src/main.js
@@ -0,0 +1,3 @@
+require('./styles/app.scss');
+
+import $ from 'jquery';
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   entry: path.resolve(__dirname, 'src/main.js'),
   output: {
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname),
     filename: 'app.js'
   },
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
         loader: 'babel-loader',
         exclude: [
           path.resolve(__dirname, 'node_modules'),
         ]
       },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
     ],
   },
+  resolve: {
+    extensions: ['.js', '.scss'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
 };
 
 module.exports = config;
