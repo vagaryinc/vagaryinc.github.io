35d287aedc963f512132262983fca2658b29c537
justinmandac <justinmandac@gmail.com>
2017-08-23 17:29:43 +0800

    Updated bundling config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
-const config = {};
+const path = require('path');
+
+const config = {
+  entry: path.resolve(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ]
+      },
+    ],
+  },
+};
 
 module.exports = config;
