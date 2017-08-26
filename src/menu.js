f75d1f1eec5e3f59fbcd8a8606979ff4bc0941d6
justinmandac <justinmandac@gmail.com>
2017-08-26 14:29:58 +0800

    Fixed bad jquery import

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Module for the #Menu element ie. the page navigation.
 */
-import $ from 'jQuery';
+import $ from 'uery';
 import HeaderMenu from './header-menu';
 
 const Menu = Object.assign({}, HeaderMenu, {
