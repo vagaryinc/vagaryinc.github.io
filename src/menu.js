00e99422cf520fa7d7e5c3ca005d30b7e950f196
justinmandac <justinmandac@gmail.com>
2017-08-26 14:32:29 +0800

    Typo fix

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Module for the #Menu element ie. the page navigation.
 */
-import $ from 'uery';
+import $ from 'jquery';
 import HeaderMenu from './header-menu';
 
 const Menu = Object.assign({}, HeaderMenu, {
